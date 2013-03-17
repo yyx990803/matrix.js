9bffdbe06b248f4abef454a5800e42babc418f25
Evan You <yyx990803@gmail.com>
2013-03-17 01:32:13 -0400

    update example/readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### work in progress
 
-A mini 3d engine using css3 matrix3d().
+[Example](http://sketch.evanyou.me/matrix/)
+
+A mini CSS 3D engine. Similar to Three.js CSS3D Renderer, but less intrusive and much lighter-weight (and of course less powerful).
--- example/example.js
+++ example/example.js
@@ -1,11 +1,16 @@
 window.addEventListener('load', function () {
 
+    // this creates a div with id="container" and appends it to body (using querySelector)
+    // you can also pass in an existing DOM node
     var container = new MX.Object3D('div#container').addTo('body')
 
+    // you can extend Object3D
     var Box = MX.Object3D.extend({
 
+        // this will be called within the contructor
         init: function (size) {
 
+            // an Object3D's associated DOM node is the "el" property
             this.el.classList.add('box')
 
             var top = new MX.Object3D('div.face')
@@ -31,13 +36,22 @@ window.addEventListener('load', function () {
             var back = new MX.Object3D('div.face')
             back.z = 50
 
+            // adding children, must also be instances of Object3D
             this.add(top, bottom, left, right, front, back)
 
             this.scale = size / 100
 
+            // this applies the updated CSS style
+            // required for any change to take effect
+            // when a parent object's update() is called
+            // all its children will be updated as well
             this.update()
+
+            // if this object's children won't move by themselves
             this.updateChildren = false
         }
+
+        // other properties will be mixed into the prototype of the new constructor
 
     })
 
@@ -47,11 +61,17 @@ window.addEventListener('load', function () {
         h = window.innerHeight
 
     for (var i = 0; i < 30; i++) {
+
         var box = new Box(Math.random() * 70 + 30)
+
+        // setting random position for each box
         box.x = Math.random() * w - w/2
         box.y = Math.random() * h - h/2
         box.z = Math.random() * 500 - 250
+
+        // simply attaching some random data here
         box.axis = axes[Math.floor(Math.random() * 3)]
+
         container.add(box)
     }
 
